44e64db3628cdb04fe7cf3e03f826cbba559d3c1
erikadelgado455 <erikadelgado455@yahoo.com>
2018-07-29 06:45:22 -0400

    i hope im doing this right ive made my array and im working on my button function and im appending the button section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
 
 
-var topics =['bears','sharks',]
+var topics =['bears','sharks','whales','goats','foxes','dogs','owls','horses','bugs',]
 
+var button=function(){
+    $('#buttonsection').empty();
+    for(var i=0;i<topics.length; i++){
+        var newbutton=$('<btn>');
+        newbutton.attr('data-type');
+        newbutton.text(topics[i]);
+        $('#buttonsection').append()
+    }
+}
 
-
+button();
 });
